e0bcf4ce333d803ff3b5d9eca9dced9cadc6692d
asakazu0314-rgb <asakazu.0314@gmail.com>
2026-08-21 20:52:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,5 @@
 // 詳しい手順は training/README.md を参照してください。
 window.BUG_TRAINING_CONFIG = {
   SUPABASE_URL: 'https://jhrxctqalsxgjpohcmog.supabase.co',
-  SUPABASE_ANON_KEY: '
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpocnhjdHFhbHN4Z2pwb2hjbW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjA3MzcsImV4cCI6MjEwMjc5NjczN30.-EaEVLY4qhZ7zCwFCv59CT-F9WHcxctBQND4LwyRbRY',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpocnhjdHFhbHN4Z2pwb2hjbW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjA3MzcsImV4cCI6MjEwMjc5NjczN30.-EaEVLY4qhZ7zCwFCv59CT-F9WHcxctBQND4LwyRbRY',
 };
